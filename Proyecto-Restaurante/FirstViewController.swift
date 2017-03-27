5f7cd37b92097eb92200feed1ccad484c2ba541e
gaaa18 <alvarenga_german@hotmail.com>
2017-03-27 11:06:24 -0700

    Initial Commit

---
+++ Proyecto-Restaurante/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Proyecto-Restaurante
+//
+//  Created by German Alfredo Alvarenga on 27/3/17.
+//  Copyright © 2017 GermanAlfredoAlvarenga. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
